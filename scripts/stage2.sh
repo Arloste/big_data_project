3ce25443ac61bd96d421f314d6ebbb78ed9e038e
Areloste <d.meshcherekov@innopolis.university>
2024-04-14 19:23:29 +0300

    Fix stage 1 (moving to server)

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -2,38 +2,27 @@
 
 # Preparation -- moving schemas to warehouse/avsc
 echo "LOGGER: Moving schemas to warehouse/avsc"
-
 password="GEEzBg4UuiWuNj0T"
-#sshpass -p $password ssh team30@hadoop-01.uni.innopolis.ru -p 22 << EOF
+
 # Remove existing folder
-#hdfs dfs -rm -R project/warehouse/avsc
+hdfs dfs -rm -R project/warehouse/avsc
 
 # Create folder for schema
-#hdfs dfs -mkdir -p project/warehouse/avsc
+hdfs dfs -mkdir -p project/warehouse/avsc
 
 # Move schema to folder
-#hdfs dfs -put *.avsc project/warehouse/avsc
-
-#exit
-#EOF
+hdfs dfs -put output/rides.avsc project/warehouse/avsc/
 
 echo "LOGGER: Schemas moved"
 
 
 # Build Hive Tables
 echo "LOGGER: Building Hive Tables"
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n teamx30 -p $password -f sql/db.hql > output/hive_results.txt 2> /dev/null
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n teamx30 -p $password -f sql/db.hql > output/hive_results.txt
 echo "LOGGER: Build tables; see output in output/hive_results.txt"
 
 
 # Performs EDA
 echo "LOGGER: EDA [1/5]..."
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team30 -p $password -f sql/q1.hql
-echo "LOGGER: EDA [2/5]..."
-
 echo "LOGGER: EDA -- ALL DONE"
-
-
-
-
-
